68fd08bd775457e952ea00c5505c8f1bb4a3a309
Anderson Vitor <andersonvittor1801@gmail.com>
2024-05-24 15:17:11 -0300

    feat(hashrouter): hashRoute

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ShopContextProvider } from './Context/ShopContext';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ShopContextProvider>
-    <React.StrictMode>
+    <HashRouter>
       <App />
-    </React.StrictMode>
+    </HashRouter>
   </ShopContextProvider>
 );
 
