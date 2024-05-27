e0939ea04f866e0dacd7b646abcc8a24c16197b1
Anderson Vitor <andersonvittor1801@gmail.com>
2024-05-26 21:14:02 -0300

    style(shop): styling shop

--- src/Pages/Shop/Product.jsx
+++ src/Pages/Shop/Product.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import * as P from './Styles'
 import { ShopContext } from '../../Context/ShopContext'
+import { ShoppingBag } from 'phosphor-react'
 
 export const Product = (props) => {
     // Objeto com todos as informacoes dos produtos que pegamos em Shop.jsx
@@ -13,16 +14,19 @@ export const Product = (props) => {
 
   return (
     <P.Product>
+      <P.ContentContainer>
         <P.img src={image}/>
 
-        <P.description>
-            <p><b>{title}</b></p>
-            <p>R$ {price}</p>
+        <P.description className='description'>
+          <p className='title'><b>{title}</b></p>
+          <p>R$ {price}</p>
         </P.description>
 
-        <P.button onClick={() => addToCart(id)}>
+      </P.ContentContainer>
+
+        <P.button onClick={() => addToCart(id)} className='button'>
           {/* Aqui usamos o cartItemAmount[id] para saber se era maior que 0, se sim mostrariamos a quantidade de items no carrinho ate o momento */}
-           Add To Cart {cartItemAmount > 0 ? `(${cartItemAmount})` : ''}
+          <ShoppingBag size={25}/>  Comprar Produto {cartItemAmount > 0 ? `(${cartItemAmount})` : ''}
         </P.button>
     </P.Product>
   )
--- src/Pages/Shop/Styles.jsx
+++ src/Pages/Shop/Styles.jsx
@@ -5,7 +5,7 @@ export const Shop = styled.div``
 export const ShopTitle = styled.div`
     margin-top: 50px;
     text-align: center;
-    font-size: 30px;
+    font-size: 25px;
 
     @media (max-width: 700px) {
         font-size: 20px;
@@ -22,38 +22,86 @@ export const Products = styled.div`
     grid-template-columns: 1fr 1fr 1fr;
     place-items: center;
     min-height: 100vh;
+    gap: 20px;
 
     @media (max-width: 700px) {
         width: 100vw;
         grid-template-columns: 1fr;
         margin-top: 10px;
     }
+
+    @media (min-width: 800px) and (max-width: 1024px) {
+        grid-template-columns: 1fr 1fr;
+    }
+
 `
 
 // Product style
 
 export const Product = styled.div`
-    border-radius: 15px;
     width: 300px;
-    height: 450px;
-    margin: 80px;
+    height: 350px;
+    margin: 30px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
     gap: 10px;
+    cursor: pointer;
+    transition: transform 0.3s ease-in-out, border 0.3s ease-in-out;
+    position: relative;
+
+    &:hover {
+        border: 1px solid #000;
+
+        .description {
+            transform: translateY(20px);
+            opacity: 0;
+        }
+
+        .button {
+            transform: translateY(-20px);
+            opacity: 1;
+        }
+    }
+
 
     @media (max-width: 700px){
         width: 90%;
         margin:0;
-        height: 400px;
+        height: 350px;
         gap: 10px;
+
+        &:active {
+            border: 1px solid #000;
+    
+            .description {
+                transform: translateY(20px);
+                opacity: 0;
+            }
+    
+            .button {
+                transform: translateY(-20px);
+                opacity: 1;
+            }
+        }
     }
 `
 
+export const ContentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+`;
+
 export const img = styled.img`
-    width: 200px;
+    width: 100%;
+    height: 250px;
+    object-fit: contain;
+    transition: transform 0.3s ease-in-out;
 
     @media (max-width: 700px) {
         width: 150px;
@@ -61,22 +109,26 @@ export const img = styled.img`
 `
 
 export const description = styled.div`
-    text-align: center;
+    font-size: 15px;
+    margin: 10px;
+    opacity: 1;
+    transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
 `
 
 export const button = styled.button`
+    display: flex;
+    align-items: center;
+    gap: 10px;
     background-color: transparent;
-    border: 2px solid rgb(19, 19, 19);
-    min-width: 100px;
-    padding-left: 10px;
-    padding-right: 10px;
-    padding-top: 5px;
-    padding-bottom: 5px;
-    border-radius: 15px;
+    border: none;
+    opacity: 0;
+    font-size: 18px;
+    font-weight: bold;
+    position: absolute;
+    bottom: 0px;
+    transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
 
     &:hover {
-        background-color: rgb(19, 19, 19);
-        color: white;
         cursor: pointer;
     }
 `
