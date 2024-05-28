9cf8363ad621e82eac482e1756b3fd2187bfe54a
Anderson Vitor <andersonvittor1801@gmail.com>
2024-05-28 17:51:19 -0300

    feat(transition): creating transition when switching among routes

---
+++ src/Components/TransitionComponent/TransitionComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { Routes, Route, useLocation } from 'react-router-dom'
+
+export const TransitionComponent = () => {
+  return (
+    <div>TransitionComponent</div>
+  )
+}
