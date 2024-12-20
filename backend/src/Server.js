28043153e9e8bffaad24336fb1242faba7d023b2
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2024-12-20 10:31:39 -0300

    feat(backend): creating indexRoute

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 2000
 
+//importando rotas
+const indexRoute = require('./Routes/index.route')
+
+//usando as rotas
+app.use('/', indexRoute)
+
 app.listen(port, () => {
     console.log("Servidor rodando na porta", port)
 })
